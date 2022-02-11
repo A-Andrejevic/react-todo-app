fdf7e388dc57938c93ad26927a7a7ee76dc85328
A-Andrejevic <92856864+A-Andrejevic@users.noreply.github.com>
2022-02-11 12:48:41 +0100

    Added React icons and edited css

--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { FiX } from "react-icons/fi";
 
 export default function Task(props) {
   return (
     <div className="Task">
-      <h4>{props.task.name}</h4>
+      <h4>{props.task.name} <FiX /> </h4>
+      <h6>{props.task.day}</h6>
     </div>
   );
 }
